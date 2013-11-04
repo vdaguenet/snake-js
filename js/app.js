8708a82b07117cb6c85f128f503499932eaee045
Taatii <valentin@MC-G-LPCc120-12.local>
2013-11-04 11:57:24 +0100

    First commit. Begining of the server and the application

---
+++ js/app.js
@@ -0,0 +1,4 @@
+var Server = require('./server.js').Server;
+
+var server = new Server(); // instancie le serveur.
+server.init(5000);
---
+++ js/server.js
@@ -0,0 +1,16 @@
+var Express = require('express'),
+	http = require('http'),
+	app = Express();
+
+// Export est l'équivalent de public (non dispo en JS)
+// Le constructeur est placé dans la variable Server. Permet de ne pas réécrire exports à chaque méthode.
+exports.Server = Server = function () {
+
+};
+
+Server.prototype.init = function(port) {
+	this.server = http.createServer(app);
+	this.server.listen(port);
+
+	console.log('Server started, listening port : ' + port);
+};
